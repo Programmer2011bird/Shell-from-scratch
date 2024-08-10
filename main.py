390aea86affe6177b9883d3e7fe32e3c462d64c4
Programmer2011bird <maysamshaker7@gmail.com>
2024-08-10 16:34:29 +0330

    type implementation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def main():
         sys.stdout.flush()
 
         COMMAND = str(input())
+        COMMANDS_LIST: list[str] = ["echo", "exit", "exit 0", "type"]
 
         match COMMAND:
             case 'exit 0':
@@ -16,9 +17,19 @@ def main():
             message = COMMAND.split("echo")[1]
             print(message)
         
-        else:
-            print(f'{COMMAND}: command not found')
+        if COMMAND.startswith('type'):
+            function = COMMAND.split("type ")[1]
 
+            if (function in COMMANDS_LIST) == False:
+                print(f'{COMMAND}: command not found')
+            
+            else:
+                message = f'{function} is a shell builtin'
+
+                print(message)
+
+        if (COMMAND.split(" ")[0] in COMMANDS_LIST) == False:
+            print(f'{COMMAND}: command not found')
 
 if __name__ == "__main__":
     main()
